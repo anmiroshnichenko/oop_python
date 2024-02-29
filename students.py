7bfb1dbe0274ea007516c3ff6452029d7959208d
Aleksandr Miroshnichenko <alexand.miroshnichenko@yandex.ru>
2024-02-29 16:39:13 +0300

    Initial commit

---
+++ students.py
@@ -0,0 +1,45 @@
+class Student:
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+
+    def add_courses(self, course_name):
+        self.finished_courses.append(course_name)
+
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached  and student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+    
+
+best_student = Student('Ruoy', 'Eman', 'your_gender')
+# best_student.finished_courses += ['Git']
+best_student.courses_in_progress += ['Python']
+# best_student.grades['Git'] = [10, 10, 10, 10, 10]
+best_student.grades['Python'] = [10, 10]
+
+# print(best_student.finished_courses)
+# print(best_student.courses_in_progress)
+# print(best_student.grades)
+cool_mentor = Mentor('Some', 'Buddy')
+cool_mentor.courses_attached += ['Python']
+
+cool_mentor.rate_hw(best_student, 'Python', 11)
+cool_mentor.rate_hw(best_student, 'Python', 6)
+cool_mentor.rate_hw(best_student, 'Python', 1)
+# print(cool_mentor.courses_attached)
+print(best_student.grades)
