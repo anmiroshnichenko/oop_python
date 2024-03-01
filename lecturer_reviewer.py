dfd63042e5de9942cf95378bf40db9989104798a
Aleksandr Miroshnichenko <alexand.miroshnichenko@yandex.ru>
2024-03-01 18:42:58 +0000

    changed lecturer_reviewer.py

--- lecturer_reviewer.py
+++ lecturer_reviewer.py
@@ -10,11 +10,30 @@ class Student:
     def add_courses(self, course_name):
         self.finished_courses.append(course_name)
 
+    def rate_lecture(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'    
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
+    
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}      
+
+class Reviewer(Mentor):
+    pass
+    # def __init__(self, name, surname):
+    #     super().__init__(name, surname)   
 
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -25,55 +44,27 @@ class Mentor:
         else:
             return 'Ошибка'
 
-class Lecturer(Mentor):
-    def __init__(self, name, surname):
-        # super().__init__(name, surname)
-        self.name = name
-        self.surname = surname
-        self.courses_attached = []
-
-class Reviewer(Mentor):
-    def __init__(self, name, surname):
-        super().__init__(name, surname)   
-
             
-best_reviewer = Lecturer('Alex', 'Jones')
+best_reviewer = Reviewer('Alex', 'Jones')
 best_reviewer.courses_attached += ['Python']
-print(best_reviewer.name, best_reviewer.surname)
-print(best_reviewer.courses_attached)
-
-
-
-# print(Reviewer.mro())
+print(best_reviewer.name, best_reviewer.surname, best_reviewer.courses_attached)
 
 bed_lecturer = Lecturer('Jon', 'Jones')
 bed_lecturer.courses_attached += ['Git', 'java']
-print(bed_lecturer.name)
-print(bed_lecturer.surname)
-print(bed_lecturer.courses_attached)
-
-
+print(bed_lecturer.name, bed_lecturer.surname, bed_lecturer.courses_attached)
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.finished_courses += ['Git']
 best_student.grades['Git'] = [10, 10, 10, 10]
 best_student.courses_in_progress += ['Python', 'java']
-best_student.grades['Python'] = [10, 10]
-bed_lecturer.rate_hw(best_student, 'java', 3)
-best_reviewer.rate_hw(best_student, 'Python', 2)
+best_student.grades['Python'] = [10, 4]
+best_reviewer.rate_hw(best_student, 'Python', 6)
 print(best_student.grades)
 
+# print(Reviewer.mro())
 # best_student.grades['Git'] = [10, 10, 10, 10, 10]
 # best_student.grades['Python'] = [10, 10]
 
 # print(best_student.finished_courses)
 # print(best_student.courses_in_progress)
-
-# cool_mentor = Mentor('Some', 'Buddy')
-# cool_mentor.courses_attached += ['Python']
-
-# cool_mentor.rate_hw(best_student, 'Python', 11)
-# cool_mentor.rate_hw(best_student, 'Python', 6)
-# cool_mentor.rate_hw(best_student, 'Python', 1)
-# print(cool_mentor.courses_attached)
 # print(best_student.grades)
