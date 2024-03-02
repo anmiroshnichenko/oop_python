0fa5c09644f84f3b6968d0a20d70f9ecd26aad9d
Aleksandr Miroshnichenko <alexand.miroshnichenko@yandex.ru>
2024-03-02 10:51:44 +0000

    added attributes and class interaction

--- lecturer_reviewer.py
+++ lecturer_reviewer.py
@@ -31,10 +31,7 @@ class Lecturer(Mentor):
         self.grades = {}      
 
 class Reviewer(Mentor):
-    pass
-    # def __init__(self, name, surname):
-    #     super().__init__(name, surname)   
-
+        
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -45,12 +42,12 @@ class Reviewer(Mentor):
             return 'Ошибка'
 
             
-best_reviewer = Reviewer('Alex', 'Jones')
-best_reviewer.courses_attached += ['Python']
-print(best_reviewer.name, best_reviewer.surname, best_reviewer.courses_attached)
+# best_reviewer = Reviewer('Alex', 'Jones')
+# best_reviewer.courses_attached += ['Python']
+# print(best_reviewer.name, best_reviewer.surname, best_reviewer.courses_attached)
 
 bed_lecturer = Lecturer('Jon', 'Jones')
-bed_lecturer.courses_attached += ['Git', 'java']
+bed_lecturer.courses_attached += ['Git', 'java', 'Python']
 print(bed_lecturer.name, bed_lecturer.surname, bed_lecturer.courses_attached)
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
@@ -58,8 +55,12 @@ best_student.finished_courses += ['Git']
 best_student.grades['Git'] = [10, 10, 10, 10]
 best_student.courses_in_progress += ['Python', 'java']
 best_student.grades['Python'] = [10, 4]
-best_reviewer.rate_hw(best_student, 'Python', 6)
-print(best_student.grades)
+# best_reviewer.rate_hw(best_student, 'Python', 6)
+# print(best_student.grades)
+best_student.rate_lecture(bed_lecturer, 'Git', 7)
+best_student.rate_lecture(bed_lecturer, 'java', 7)
+best_student.rate_lecture(bed_lecturer, 'Python', 3)
+print(bed_lecturer.grades)
 
 # print(Reviewer.mro())
 # best_student.grades['Git'] = [10, 10, 10, 10, 10]
