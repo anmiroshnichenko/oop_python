069b52395efe132917af6ac29cbad8d23c210aab
Aleksandr Miroshnichenko <alexand.miroshnichenko@yandex.ru>
2024-03-04 17:54:58 +0000

    changed read_file.py

--- read_file.py
+++ read_file.py
@@ -1,12 +1,14 @@
 cook_book = {} ## Создаем пустой словарь
 with open('recipes.txt') as f: #Читаем файл
-    lines = f.read().splitlines() # read().splitlines() - чтобы небыло пустых строк
-    # print(lines)
-    for line in lines: # Проходимся по каждой строчке
-        # key, value = line.split(': ') # Разделяем каждую строку по двоеточии(в key будет - пицца, в value - 01)
-        key, value = line.split()
-        cook_book.update({key:value})	 # Добавляем в словарь
-
+    # lines = f.read().splitlines() # read().splitlines() - чтобы небыло пустых строк
+    # print(lines)   
+    for line in f: # Проходимся по каждой строчке
+        print(line.strip().replace("|", "")) #обрежет строку с обоих концов. Если аргумент не задан удалит пробелы с обоих концов строки
+        # print(line.strip())
+    #     # key, value = line.split(': ') # Разделяем каждую строку по двоеточии(в key будет - пицца, в value - 01)
+    #     key, value = line.split()
+    #     cook_book.update({key:value})	 # Добавляем в словарь
+    
         # print(line)
     # lines = f.readlines()
     # print(lines[0])
